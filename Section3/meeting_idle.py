70a5d2fac1b950c09cdd404b1d6cc05666cac96f
Dave Shields <66278753+shieldskcd@users.noreply.github.com>
2021-11-18 23:44:04 -0600

    Adding Section 3 exercise

---
+++ Section3/meeting_idle.py
@@ -0,0 +1,8 @@
+first_number = 1+1+5 # This is an expression
+print(first_number) # Python opens the box and prints it
+
+second_number = 105+10 # This is another expression
+print(second_number) # Python opens this box and prints it
+
+total = first_number + second_number # Adds these two boxes
+print(total) # Python prints the data it found in the new box.
